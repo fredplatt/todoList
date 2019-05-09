4b952c0f23a88c638737cd250066fa39d51537ea
fredplatt <fred_platt@hotmail.com>
2019-05-09 10:49:31 +0100

    inital commit

---
+++ src/Classes/Controllers/HomePageController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Todo\Classes;
+
+use Slim\Views\PhpRenderer;
+
+class HomePageController {
+    public $renderer;
+    public $listModel;
+
+    public function __construct(PhpRenderer $renderer, $listModel)
+    {
+        $this->renderer = $renderer;
+        $this->listModel = $listModel;
+    }
+
+    public function __invoke($request, $response, $args)
+    {
+        
+    }
+}
